c858699f2165e4f4e07a5d9b9663d16a65328520
Ten_IO <194474940+Ten-IO@users.noreply.github.com>
2025-09-08 00:17:32 +0700

    revise: recursive XOR

--- Revision/03-pointer.cpp
+++ Revision/03-pointer.cpp
@@ -1,3 +1,18 @@
 /*
     [+] Q3. Write a C++ program using pointers to swap two numbers.
 */
+#include <iostream>
+void swap(int *a, int *b)
+{
+    int temp = *a ^ *b;
+    *a = *a ^ temp;
+    *b = *b ^ temp;
+}
+int main()
+{
+    int n1 = 2, n2 = 5;
+    std::cout << "Origin: [a] " << n1 << " [b] " << n2 << std::endl;
+    swap(&n1, &n2);
+    std::cout << "Swap:   [a] " << n1 << " [b] " << n2;
+    return 0;
+}
